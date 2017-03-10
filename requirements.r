4bc4032edfae14f4469110fd156c7b4f2332fb7b
Filip Schouwenaars <filip@datacamp.com>
2017-03-10 11:39:33 +0100

    no /home/repl

--- requirements.r
+++ requirements.r
@@ -2,4 +2,4 @@
 # install.packages("fivethirtyeight")
 # install.packages("ggplot2")
 # install.packages("dplyr")
-download.file("http://s3.amazonaws.com/assets.datacamp.com/staging/course_2406/datasets/iris.csv", destfile = "/home/repl/.iris.csv")
+download.file("http://s3.amazonaws.com/assets.datacamp.com/staging/course_2406/datasets/iris.csv", destfile = ".iris.csv")
